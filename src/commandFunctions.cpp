e820c794f89983cd3d138cc811de5cc4752c690d
Philipp Hofmann <phhofman@3-F-13.42heilbronn.de>
2025-11-27 13:42:15 +0100

    changed commandHandler

--- src/commandFunctions.cpp
+++ src/commandFunctions.cpp
@@ -1,19 +1,19 @@
 #include "commandFunctions.hpp"
 #include "Client.hpp"
 
-void handlePass(Client &client, const std::vector<std::string> &args)
+void handlePass(Client &client, const IrcMsg &msg)
 {
     if (client.get_registered())
     {
         client.sendMessage("462 :You may not reregister");
         return;
     }
-    if (args.size() < 2)
+    if (msg.get_params().size() < 2)
     {
         client.sendMessage("461 PASS :Not enough parameters");
         return;
     }
-    std::string pass = args[1];
+    std::string pass = msg.get_params()[0];
 
     // if (pass != server.getPass())
     //{
@@ -24,7 +24,7 @@ void handlePass(Client &client, const std::vector<std::string> &args)
     // noch nicht registriert, da Nick noch nicht gesetzt
 }
 
-void handleNick(Client &client, const std::vector<std::string> &args)
+void handleNick(Client &client, const IrcMsg &msg)
 {
     if (args.size() < 2)
     {
@@ -40,13 +40,13 @@ void handleNick(Client &client, const std::vector<std::string> &args)
     // wenn ok, nickname setten fuer client
 }
 
-void handleUser(Client &client, const std::vector<std::string> &args)
+void handleUser(Client &client, const IrcMsg &msg)
 {
     // USER <username> <hostname> <servername> :<realname>
     // check ich nciht
 }
 
-void handleOper(Client &client, const std::vector<std::string> &args)
+void handleOper(Client &client, const IrcMsg &msg)
 {
     // OPER <username> <password>
     // checken ob 3 in arg
@@ -56,20 +56,20 @@ void handleOper(Client &client, const std::vector<std::string> &args)
     // msg senden
 }
 
-void handleMode(Client &client, const std::vector<std::string> &args)
+void handleMode(Client &client, const IrcMsg &msg)
 {
     // MODE <channel> <modes> [parameters]
     // kein plan
 }
 
-void handleQuit(Client &client, const std::vector<std::string> &args)
+void handleQuit(Client &client, const IrcMsg &msg)
 {
     // braodcast to channel
     // remove Client from channel
     // disconnect Client
 }
 
-void handleJoin(Client &client, const std::vector<std::string> &args)
+void handleJoin(Client &client, const IrcMsg &msg)
 {
     // check ob arg groesser als 2
     // channle name muss mit # anfangen?
@@ -78,7 +78,7 @@ void handleJoin(Client &client, const std::vector<std::string> &args)
     // boradcast join
 }
 
-void handleTopic(Client &client, const std::vector<std::string> &args)
+void handleTopic(Client &client, const IrcMsg &msg)
 {
     // arg muss groeser gleich 2 sein
     //  arg[1] = channel name
@@ -88,7 +88,7 @@ void handleTopic(Client &client, const std::vector<std::string> &args)
     // if arg == 3 topic setzen (in channel struct) ((setter und getter func benutzen))
 }
 
-void handleKick(Client &client, const std::vector<std::string> &args)
+void handleKick(Client &client, const IrcMsg &msg)
 {
     // arg muss groesser gleich 3 sein
     // arg[1] channel name
@@ -102,7 +102,7 @@ void handleKick(Client &client, const std::vector<std::string> &args)
     // Target aus Channel entfernen
 }
 
-void privMsg(Client &client, const std::vector<std::string> &args)
+void privMsg(Client &client, const IrcMsg &msg)
 {
     // arg muss groesser gleich 3 sein
     // arg[1] channel name
